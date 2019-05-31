0bd81d611ba3faacfd6131ff33cf8b618bbf0c4d
Madaragog <cedric.dorez@gmail.com>
2019-05-31 08:36:12 +0200

    creation of the class Question in the file Question.swift

---
+++ OpenQuiz/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Questions.swift
+//  OpenQuiz
+//
+//  Created by Cedric on 31/05/2019.
+//  Copyright © 2019 Cedric. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    var title = ""
+    var isCorrect = false
+}
